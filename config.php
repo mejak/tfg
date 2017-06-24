3b59626b13fdae74753ab44c93b9fb64f314b5a9
mejak <mejakmargarian@gmail.com>
2017-06-24 12:37:32 +0200

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-define("BASE_URL", "https://tfg2.dassmedia.com");
-define("DB_HOST", "localhost");
-define("DB_NAME", "dassmedia_tfg2");
-define("DB_USER", "tfg2");
-define("DB_PASSWORD", "kr_4x7O5");
